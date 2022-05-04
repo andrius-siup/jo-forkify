d1a348aa4ad445b2d2abd914aab0378a81365770
andrius-siup <andrius.siupinys@gmail.com>
2022-05-04 11:56:08 +0100

    Fixed Bug - if there is quantity then simply convert that if no leave empty string

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2341,7 +2341,7 @@ class RecipeView {
               <svg class="recipe__icon">
                 <use href="${_iconsSvgDefault.default}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${new _fractional.Fraction(ing.quantity).toString()}</div>
+              <div class="recipe__quantity">${ing.quantity ? new _fractional.Fraction(ing.quantity).toString() : ''}</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
